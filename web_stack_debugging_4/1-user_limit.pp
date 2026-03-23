11d6a39694897d8103dd22772b9261166632549f
duwase7 <d.uwase7@alustudent.com>
2026-03-23 15:14:19 +0200

    create

---
+++ web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,10 @@
+# Increases hard and soft file limits for the holberton user
+exec { 'increase-hard-file-limit':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/',
+}
+
+exec { 'increase-soft-file-limit':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/',
+}
